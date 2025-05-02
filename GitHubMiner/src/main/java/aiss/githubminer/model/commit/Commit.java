e9b310093e91dcb2f95e7a9df36ce408144acfa5
Yabarta <perezsornipablo@gmail.com>
2025-05-02 12:22:39 +0200

    GitHubMiner arreglado

---
+++ GitHubMiner/src/main/java/aiss/githubminer/model/commit/Commit.java
@@ -0,0 +1,106 @@
+
+package aiss.githubminer.model.commit;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Commit {
+
+    @JsonProperty("sha") // sha es el id del commit
+    private String sha;
+    @JsonProperty("commit")
+    private Commit__1 commit;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    @JsonProperty("author")
+    private Author__1 author;
+    @JsonProperty("committer")
+    private Committer__1 committer;
+
+    @JsonProperty("sha")
+    public String getSha() {
+        return sha;
+    }
+
+    @JsonProperty("sha")
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    @JsonProperty("commit")
+    public Commit__1 getCommit() {
+        return commit;
+    }
+
+    @JsonProperty("commit")
+    public void setCommit(Commit__1 commit) {
+        this.commit = commit;
+    }
+
+    @JsonProperty("html_url")
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    @JsonProperty("author")
+    public Author__1 getAuthor() {
+        return author;
+    }
+
+    @JsonProperty("author")
+    public void setAuthor(Author__1 author) {
+        this.author = author;
+    }
+
+    @JsonProperty("committer")
+    public Committer__1 getCommitter() {
+        return committer;
+    }
+
+    @JsonProperty("committer")
+    public void setCommitter(Committer__1 committer) {
+        this.committer = committer;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Commit.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("sha");
+        sb.append('=');
+        sb.append(((this.sha == null)?"<null>":this.sha));
+        sb.append(',');
+        sb.append("commit");
+        sb.append('=');
+        sb.append(((this.commit == null)?"<null>":this.commit));
+        sb.append(',');
+        sb.append("htmlUrl");
+        sb.append('=');
+        sb.append(((this.htmlUrl == null)?"<null>":this.htmlUrl));
+        sb.append(',');
+        sb.append("author");
+        sb.append('=');
+        sb.append(((this.author == null)?"<null>":this.author));
+        sb.append(',');
+        sb.append("committer");
+        sb.append('=');
+        sb.append(((this.committer == null)?"<null>":this.committer));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
