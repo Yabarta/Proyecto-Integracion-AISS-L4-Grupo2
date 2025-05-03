15de56ccfb4eddbeae967588f261806e10b2c4ec
Yabarta <perezsornipablo@gmail.com>
2025-05-03 13:38:12 +0200

    Modelos limpios de bitbucket + Esqueleto de gitMiner

---
+++ BitbucketMiner/src/main/java/aiss/bitbucketminer/model/Links__2.java
@@ -0,0 +1,71 @@
+
+package aiss.bitbucketminer.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Links__2 {
+
+    @JsonProperty("avatar")
+    private Avatar__2 avatar;
+    @JsonProperty("html")
+    private Html__2 html;
+    @JsonProperty("self")
+    private Self__2 self;
+
+    @JsonProperty("avatar")
+    public Avatar__2 getAvatar() {
+        return avatar;
+    }
+
+    @JsonProperty("avatar")
+    public void setAvatar(Avatar__2 avatar) {
+        this.avatar = avatar;
+    }
+
+    @JsonProperty("html")
+    public Html__2 getHtml() {
+        return html;
+    }
+
+    @JsonProperty("html")
+    public void setHtml(Html__2 html) {
+        this.html = html;
+    }
+
+    @JsonProperty("self")
+    public Self__2 getSelf() {
+        return self;
+    }
+
+    @JsonProperty("self")
+    public void setSelf(Self__2 self) {
+        this.self = self;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Links__2 .class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("avatar");
+        sb.append('=');
+        sb.append(((this.avatar == null)?"<null>":this.avatar));
+        sb.append(',');
+        sb.append("html");
+        sb.append('=');
+        sb.append(((this.html == null)?"<null>":this.html));
+        sb.append(',');
+        sb.append("self");
+        sb.append('=');
+        sb.append(((this.self == null)?"<null>":this.self));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
