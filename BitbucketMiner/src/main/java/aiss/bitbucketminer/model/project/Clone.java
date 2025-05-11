e3f202a6485810769f5666ab60b6c0fceecccd11
Pablo <pablopmar3@gmail.com>
2025-05-11 18:58:45 +0200

    No he subido esto en el anterior commit

---
+++ BitbucketMiner/src/main/java/aiss/bitbucketminer/model/project/Clone.java
@@ -0,0 +1,62 @@
+
+package aiss.bitbucketminer.model.project;
+
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "href"
+})
+@Generated("jsonschema2pojo")
+public class Clone {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("href")
+    private String href;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("href")
+    public String getHref() {
+        return href;
+    }
+
+    @JsonProperty("href")
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Clone.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("href");
+        sb.append('=');
+        sb.append(((this.href == null)?"<null>":this.href));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
---
+++ BitbucketMiner/src/main/java/aiss/bitbucketminer/model/project/Links__2.java
@@ -0,0 +1,79 @@
+
+package aiss.bitbucketminer.model.project;
+
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "avatar",
+    "html",
+    "self"
+})
+@Generated("jsonschema2pojo")
+public class Links__2 {
+
+    @JsonProperty("avatar")
+    private Avatar__2 avatar;
+    @JsonProperty("html")
+    private Html__2 html;
+    @JsonProperty("self")
+    private Self__2 self;
+
+    @JsonProperty("avatar")
+    public Avatar__2 getAvatar() {
+        return avatar;
+    }
+
+    @JsonProperty("avatar")
+    public void setAvatar(Avatar__2 avatar) {
+        this.avatar = avatar;
+    }
+
+    @JsonProperty("html")
+    public Html__2 getHtml() {
+        return html;
+    }
+
+    @JsonProperty("html")
+    public void setHtml(Html__2 html) {
+        this.html = html;
+    }
+
+    @JsonProperty("self")
+    public Self__2 getSelf() {
+        return self;
+    }
+
+    @JsonProperty("self")
+    public void setSelf(Self__2 self) {
+        this.self = self;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Links__2 .class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("avatar");
+        sb.append('=');
+        sb.append(((this.avatar == null)?"<null>":this.avatar));
+        sb.append(',');
+        sb.append("html");
+        sb.append('=');
+        sb.append(((this.html == null)?"<null>":this.html));
+        sb.append(',');
+        sb.append("self");
+        sb.append('=');
+        sb.append(((this.self == null)?"<null>":this.self));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
